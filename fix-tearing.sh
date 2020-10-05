da3a81de8e57276880b6beb505542fe9b5d9461d
Victor <victor.ananias8@gmail.com>
2020-10-05 17:29:11 -0300

    wip

--- fix-tearing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo touch /etc/modprobe.d/nvidia-drm-nomodeset.conf
-echo 'options nvidia-drm modeset=1' | sudo tee -a /etc/modprobe.d/nvidia-drm-nomodeset.conf
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOWNLOADS="Installation Files"
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_UNITY_HUB="https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage"
+
+# PACKAGES
+APT_PACKAGES="curl snapd zsh git fonts-firacode vlc steam remmina remmina-plugin-rdp php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath"
+SNAP_PACKAGES="spotify code insomnia dotnet-runtime-31 dotnet-sdk krita blender"
+
+# UPDATE
+sudo apt update -y
+sudo apt upgrade -y
+
+for i in $APT_PACKAGES; do
+  sudo apt install -y $i
+done
+
+# install snaps
+for i in $APT_PACKAGES; do
+  sudo snap install $i
+done
+
+cd /home/$USER/Downloads
+mkdir "$DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME" -P "$DOWNLOADS"
+wget -c "$URL_UNITY_HUB" -P "$DOWNLOADS"
