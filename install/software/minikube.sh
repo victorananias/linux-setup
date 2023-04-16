e5055aa104a3d300561db70d7f7b8db5b83bc15e
Victor <victor@ananias.dev>
2023-04-16 10:30:06 -0300

    update

---
+++ install/software/minikube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+
+sudo dpkg -i minikube_latest_amd64.deb
+
+rm minikube_latest_amd64.deb
---
+++ scripts/install-gnome-dracula-theme.sh
@@ -0,0 +1,9 @@
+#!/bin/bas
+
+sudo apt-get install dconf-cli
+
+git clone https://github.com/dracula/gnome-terminal
+
+cd gnome-terminal
+
+./install.sh
